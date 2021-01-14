f61d710cb2bb2daf2cbb1262e3da549639a05226
Your Name <corespiderpro@gmail.com>
2021-01-14 22:52:25 +0530

    first commit

---
+++ Encrypt_Razor_Decrypt_Controller/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Encrypt_Razor_Decrypt_Controller.Models;
+namespace Encrypt_Razor_Decrypt_Controller.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetData(string enc)
+        {
+            var DecryptVal = Utility.DecryptStringAES(enc);
+           
+            return Json(DecryptVal, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Encrypt_Razor_Decrypt_Controller/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Encrypt_Razor_Decrypt_Controller.MvcApplication" Language="C#" %>
